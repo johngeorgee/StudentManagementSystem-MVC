e33a7f457b459fee9b9d9751668d35ed672bd9f0
johngeorgee <johngeorgeea@gmail.com>
2026-09-18 15:26:54 +0300

    feat(viewmodels): add EditDepartment view model with validation

---
+++ Models/ViewModels/EditDepartment.cs
@@ -0,0 +1,9 @@
+namespace Project.Models.ViewModels;
+
+public class EditDepartment
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ManagerName { get; set; }
+
+}
