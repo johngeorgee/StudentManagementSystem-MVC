dfb99937ade3517c5b457d15b565ae2dd68f980f
johngeorgee <johngeorgeea@gmail.com>
2026-09-24 20:19:37 +0300

    feat(repository): add TraineeRepository with eager-loaded details query
    - Inherit GenericRepository<Trainee> and implement ITraineeRepository
    - Add GetTraineeDetails including CourseResults and Department

--- Repository/TraineeRepository.cs
+++ Repository/TraineeRepository.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Project.Data;
+using Project.Interfaces;
+using Project.Models;
+
 namespace Project.Repository;
 
-public class TraineeRepository
+public class TraineeRepository: GenericRepository<Trainee>, ITraineeRepository
 {
-    
+    private readonly AppDbContext _context;
+
+    public TraineeRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+        
+    }
+
+    public IQueryable<Trainee> GetTraineeDetails()
+    {
+        return  _context.Trainees
+                           .Include(c => c.CourseResults)
+                           .Include(d => d.Department);
+    }
 }
