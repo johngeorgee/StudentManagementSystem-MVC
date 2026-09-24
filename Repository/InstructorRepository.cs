d2ff89d004bf6b8ac964caf54086e4511af2c00b
johngeorgee <johngeorgeea@gmail.com>
2026-09-24 20:38:38 +0300

    feat(repository): add InstructorRepository with eager-loaded details query
    - Inherit GenericRepository<Instructor> and implement IInstructorRepository
    - Add GetInstructorDetails including Department and Course

--- Repository/InstructorRepository.cs
+++ Repository/InstructorRepository.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging.Core;
+using Project.Data;
+using Project.Interfaces;
+using Project.Models;
+
 namespace Project.Repository;
 
-public class InstructorRepository
+public class InstructorRepository : GenericRepository<Instructor>, IInstructorRepository
 {
-    
+    private readonly AppDbContext _context;
+
+    public InstructorRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public IQueryable<Instructor> GetInstructorDetails()
+    {
+       return  _context.Instructors
+            .Include(d => d.Department)
+            .Include(c => c.Course);
+    }
 }
